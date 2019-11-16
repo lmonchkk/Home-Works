fc69d16663cd25f56be6354799ee97c16202b911
unknown <lmonchkk@gmail.com>
2019-11-16 14:03:11 +0200

    class work

---
+++ JavaScript/lesson 17/script(homework).js
@@ -0,0 +1,29 @@
+'use  strict';
+let seconds = 60;
+let minutes = 60;
+let hours = 3;
+let threehours = hours * minutes * seconds;
+console.log(threehours + "секунд");
+
+
+var num;
+num = (((1 + 12 - 14) * 5) / 7) + 1 - 1;
+
+
+let a = 10;
+let b = 2;
+let c = 5;
+console.log(a + b + c);
+
+
+let a1 = 17;
+let b1 = 10;
+let c1 = a1 - b1;
+let d1 = 7;
+let result = c1 + d1;
+console.log(result);
+
+
+
+let name = "Влад";
+console.log("'Привет, " +  '%' + name + '%' + "'");
